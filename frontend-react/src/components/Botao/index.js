08aededd0cc244d7b9f3dc0e080c180f65b6613f
rafaelcorrea26 <rafael.fs.camaqua@gmail.com>
2023-09-07 18:25:08 -0300

    main

---
+++ frontend-react/src/components/Botao/index.js
@@ -0,0 +1,11 @@
+import './Botao.css';
+
+function Botao() {
+    return (
+        <div className="botao">
+            <button type="submit"> + </button>
+        </div>
+    );
+}
+
+export default Botao;
